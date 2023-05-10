44f5b7c41f320f247db0d3db2336d77254cacb34
isomorpheric <ecam900@gmail.com>
2023-05-10 01:47:17 -0400

    docs: add comments

--- src/fetchers/getCatFact.ts
+++ src/fetchers/getCatFact.ts
@@ -1,3 +1,4 @@
+// getCatFact should return a random cat fact. It should revalidate in the server every 1 second after any new visit to the route. It however does not. Why?
 export default async function getCatFact() {
   const res = await fetch('https://catfact.ninja/fact', { next: { revalidate: 1 } });
   return (await res
