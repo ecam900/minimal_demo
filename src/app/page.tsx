97520f2f73153d7485a6cf55888ab83fd4df15a9
isomorpheric <ecam900@gmail.com>
2023-05-10 01:12:25 -0400

    content: minor edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className='container max-w-6xl mx-auto py-6'>
       <Link href='/catfact'>
         <div className='p-2 bg-black text-white hover:cursor-pointer rounded-sm max-w-fit'>
-          <p>Go To Test Page</p>
+          <p>Go Test It</p>
         </div>
       </Link>
     </main>
