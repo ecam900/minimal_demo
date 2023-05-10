409f9ab51eb2c38a25cd9b4c50c6f9b045894249
Eric Crescioni <ecam900@gmail.com>
2023-05-10 02:31:24 -0400

    Update CatFactMenu.tsx

--- src/app/catfact/CatFactMenu.tsx
+++ src/app/catfact/CatFactMenu.tsx
@@ -6,7 +6,7 @@ export default function CatFactMenu() {
   return (
     <div className='w-full py-2 bg-white border rounded px-2 border-black'>
       <ul className='container flex flex-col space-y-4'>
-        <NavItem href='/catfact' text='Cat Fact (Should revalidate every second)' />
+        <NavItem href='/catfact' text='Cat Fact (revalidate:1)' />
         <NavItem href='/catfact/otherpage' text='go to /catfact/otherpage' />
       </ul>
     </div>
