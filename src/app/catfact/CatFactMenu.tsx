63d88a83bdce06fb99a25081017a1d7f7c69c042
isomorpheric <ecam900@gmail.com>
2023-05-11 21:10:12 -0400

    chore: clean up

--- src/app/catfact/CatFactMenu.tsx
+++ src/app/catfact/CatFactMenu.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { FC } from 'react';
+import NavItem from './NavItem';
 
 export default function CatFactMenu() {
   return (
@@ -13,28 +14,3 @@ export default function CatFactMenu() {
     </div>
   );
 }
-
-const NavItemColorOpts = {
-  blue: 'sky-500',
-  yellow: 'yellow-500',
-} as const;
-
-type NavItemColor = (typeof NavItemColorOpts)[keyof typeof NavItemColorOpts];
-
-type NavItemProps = {
-  href: string;
-  text: string;
-  color?: NavItemColor;
-};
-
-const NavItem: FC<NavItemProps> = ({ href, text, color = 'sky-500' }) => {
-  return (
-    <Link href={href}>
-      <li
-        className={`hover:text-sky-500  transition-all duration-300 hover:cursor-pointer hover:font-bold hover:bg-sky-500/10 w-full px-2 py-2 rounded border-l-4 border-${color}`}
-      >
-        {text}
-      </li>
-    </Link>
-  );
-};
